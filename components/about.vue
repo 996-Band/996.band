9120ef34df2ea5a3c6490eb6f07eb0263faba2dd
完颜 <wanyan.wz@B-Q2LPQ05P-0300.local>
2022-04-11 16:02:56 +0800

    update new cover photo

--- components/about.vue
+++ components/about.vue
@@ -1,6 +1,6 @@
 <template>
   <BandSection class="about">
-    <div class="photo" style="background-image: url(/images/99666.jpg);"></div>
+    <div class="photo" style="background-image: url(/images/996-group.jpg);"></div>
     <div class="intro">
       <h2>乐队介绍</h2>
       <p>996 乐队由五位前端工程师组成，核心团员最早于 2013 年在手机淘宝共事并相互结识，之后于 2016 年组团成功。乐队坚持为国内广大的程序员和上班族创作歌曲。坚持 Live 表演。</p>
@@ -22,7 +22,7 @@ export default {
 
 <style scoped>
 .about { overflow: hidden; }
-.photo { height: 75vw; background-size: contain; background-position: center center; background-repeat: no-repeat; }
+.photo { height: 75vw; background-size: contain; background-position: center top; background-repeat: no-repeat; -webkit-filter: grayscale(100%); }
 .intro { position: absolute; left: 5vw; right: 10vw; bottom: 10vh; text-shadow: 3px 3px 3px #333; }
 h2 { font-size: 4vw; margin: 0 0 1em; }
 p { font-size: 2vw; margin: 0 0 1.5em; }
